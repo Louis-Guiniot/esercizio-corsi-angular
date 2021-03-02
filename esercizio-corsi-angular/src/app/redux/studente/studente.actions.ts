1309f118639592ef165d012732d2f0e257ca2111
Pietro Locatelli <pietro.locatelli@stu.fondazionejobsacademy.org>
2021-03-02 17:02:09 +0100

    effects bug

--- esercizio-corsi-angular/src/app/redux/studente/studente.actions.ts
+++ esercizio-corsi-angular/src/app/redux/studente/studente.actions.ts
@@ -5,5 +5,5 @@ import { Response } from "src/app/core/model/Response.interface";
 export const initStudents = createAction('[Student] init students', props<{response: Response}>());
 // export const deleteStudent = createAction('[Student] delete student',props<{id: string}>());
 // export const updateStudent = createAction('[Student] find-update', props<{id:string, ragioneSociale:string, partitaIva:string, email:string, sede: string, residenza: string, name:string}>())
- export const createStudent = createAction('[Student] creazione student', props<{nome:string,cognome:string,dataNascita:string,sesso:string,idCorso:string}>());
-// export const retreiveAllStudents = createAction('[Student] student');
+export const createStudent = createAction('[Student] creazione student', props<{nome:string,cognome:string,dataNascita:string,sesso:string,idCorso:string}>());
+export const retreiveAllStudents = createAction('[Student] student');
