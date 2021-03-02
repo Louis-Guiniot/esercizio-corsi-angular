9e92425623d293ce7e89844866eb15a82d287915
Louis-Guiniot <louis99679@gmail.com>
2021-03-02 16:51:07 +0100

    studente grafica ok. maschere create

--- esercizio-corsi-angular/src/app/features/studente/main/studente.component.ts
+++ esercizio-corsi-angular/src/app/features/studente/main/studente.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NgbCalendar, NgbDate, NgbDateStruct, NgbInputDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-studente',
@@ -10,14 +10,48 @@ import { NgbCalendar, NgbDate, NgbDateStruct, NgbInputDatepickerConfig } from '@
 export class StudenteComponent implements OnInit {
 
   createStudent : FormGroup
+  updateStudent : FormGroup
 
-  constructor(private fb: FormBuilder) {
+  closeResult = ''
+
+  // paginazione
+  collectionSize:number
+  page = 1;
+  pageSize = 2;
+
+  constructor(private fb: FormBuilder, private modalService: NgbModal) {
       
+  }
+
+  open(content) {
+    this.modalService.open(content, { size: 'm'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
   ngOnInit(): void {
 
     this.createStudent = this.fb.group({
+      nome: ['', Validators.required],
+      cognome: ['', Validators.required],
+      dataNascita: ['', Validators.required],
+      sesso: ['', Validators.required],
+      idcorso: ['', Validators.required],
+    })
+
+    this.updateStudent = this.fb.group({
       nome: ['', Validators.required],
       cognome: ['', Validators.required],
       dataNascita: ['', Validators.required],
